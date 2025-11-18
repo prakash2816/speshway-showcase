ef097df56f22a023dd2d70fdb0e791ae325db1ef
prakash2816 <jayaprakashmekala56@gmail.com>
2025-11-18 21:25:36 +0530

    Update team.js

--- backend/routes/team.js
+++ backend/routes/team.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const router = express.Router();
+
 const {
-  getTeamMembers,
-  getTeamMember,
-  createTeamMember,
-  updateTeamMember,
-  deleteTeamMember
-} = require('../controllers/teamController');
-const { protect, admin } = require('../middleware/authMiddleware');
+  getTeamMembersDynamo,
+  getTeamMemberDynamo,
+  createTeamMemberDynamo,
+  updateTeamMemberDynamo,
+  deleteTeamMemberDynamo
+} = require('../controllers/teamControllerDynamo');
+
+const { protectDynamo, adminDynamo } = require('../middleware/authMiddlewareDynamo');
 const { uploadTeamImage } = require('../config/cloudinary');
 
-router.route('/').get(getTeamMembers).post(protect, admin, uploadTeamImage.single('image'), createTeamMember);
-router.route('/:id').get(getTeamMember).put(protect, admin, uploadTeamImage.single('image'), updateTeamMember).delete(protect, admin, deleteTeamMember);
+// Public Route
+router
+  .route('/')
+  .get(getTeamMembersDynamo)
+  .post(protectDynamo, adminDynamo, uploadTeamImage.single('image'), createTeamMemberDynamo);
+
+// Admin Routes
+router
+  .route('/:id')
+  .get(getTeamMemberDynamo)
+  .put(protectDynamo, adminDynamo, uploadTeamImage.single('image'), updateTeamMemberDynamo)
+  .delete(protectDynamo, adminDynamo, deleteTeamMemberDynamo);
 
 module.exports = router;
-
