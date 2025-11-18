2a8f5155c3196b62c20feb4522013aa4954aecbc
prakash2816 <jayaprakashmekala56@gmail.com>
2025-11-18 21:24:50 +0530

    Update services.js

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getServices,
-  getService,
-  createService,
-  updateService,
-  deleteService
-} = require('../controllers/serviceController');
-const { protect, admin } = require('../middleware/authMiddleware');
 
-router.route('/').get(getServices).post(protect, admin, createService);
-router.route('/:id').get(getService).put(protect, admin, updateService).delete(protect, admin, deleteService);
+const {
+  getServicesDynamo,
+  getServiceDynamo,
+  createServiceDynamo,
+  updateServiceDynamo,
+  deleteServiceDynamo
+} = require('../controllers/serviceControllerDynamo');
+
+const { protectDynamo, adminDynamo } = require('../middleware/authMiddlewareDynamo');
+
+// Public Routes
+router
+  .route('/')
+  .get(getServicesDynamo)
+  .post(protectDynamo, adminDynamo, createServiceDynamo);
+
+// Admin protected routes
+router
+  .route('/:id')
+  .get(getServiceDynamo)
+  .put(protectDynamo, adminDynamo, updateServiceDynamo)
+  .delete(protectDynamo, adminDynamo, deleteServiceDynamo);
 
 module.exports = router;
-
