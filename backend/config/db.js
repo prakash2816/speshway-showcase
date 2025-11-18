9c30c38d8b3f7296cf7e01fb6a307dad1ea5124f
prakash2816 <jayaprakashmekala56@gmail.com>
2025-11-18 20:34:45 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,29 @@
-const mongoose = require('mongoose');
+const {
+  DynamoDBClient
+} = require("@aws-sdk/client-dynamodb");
 
-const connectDB = async () => {
+const {
+  DynamoDBDocumentClient
+} = require("@aws-sdk/lib-dynamodb");
+
+// Create DynamoDB client
+const connectDB = () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const client = new DynamoDBClient({
+      region: process.env.AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+      }
+    });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const ddb = DynamoDBDocumentClient.from(client);
+
+    console.log("DynamoDB Connected Successfully");
+
+    return ddb; // return DynamoDB instance to use in controllers
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error("DynamoDB Connection Error:", error.message);
     process.exit(1);
   }
 };
