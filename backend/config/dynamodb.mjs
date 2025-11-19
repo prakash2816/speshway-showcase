56d84d323522b1ee74657c116e7bc7a61c3028c8
prakash2816 <jayaprakashmekala56@gmail.com>
2025-11-19 13:25:34 +0530

    Update and rename dynamodb.js to dynamodb.mjs

--- backend/config/dynamodb.mjs
+++ backend/config/dynamodb.mjs
@@ -1,20 +1,14 @@
-const {
-  DynamoDBClient
-} = require("@aws-sdk/client-dynamodb");
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const {
-  DynamoDBDocumentClient
-} = require("@aws-sdk/lib-dynamodb");
-
-// Create DynamoDB client
 const connectDB = () => {
   try {
     const client = new DynamoDBClient({
       region: process.env.AWS_REGION,
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-      }
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
     });
 
     const ddb = DynamoDBDocumentClient.from(client);
@@ -28,4 +22,4 @@ const connectDB = () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
