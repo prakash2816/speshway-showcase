6c9de61cb715536139cbdbc3b88736effca029bd
prakash2816 <jayaprakashmekala56@gmail.com>
2025-11-19 13:42:54 +0530

    Update dynamodb.js

--- backend/config/dynamodb.js
+++ backend/config/dynamodb.js
@@ -1,25 +1,16 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
 
 const connectDB = () => {
-  try {
-    const client = new DynamoDBClient({
-      region: process.env.AWS_REGION,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      },
-    });
+  const client = new DynamoDBClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
 
-    const ddb = DynamoDBDocumentClient.from(client);
-
-    console.log("DynamoDB Connected Successfully");
-
-    return ddb; // return DynamoDB instance to use in controllers
-  } catch (error) {
-    console.error("DynamoDB Connection Error:", error.message);
-    process.exit(1);
-  }
+  return DynamoDBDocumentClient.from(client);
 };
 
-export default connectDB;
+module.exports = connectDB;
