2920e1cfe6aaae347aeaeaac168645a1578356b9
prakash2816 <jayaprakashmekala56@gmail.com>
2025-11-19 21:02:05 +0530

    Update portfolioControllerDynamo.js

--- backend/controllers/portfolioControllerDynamo.js
+++ backend/controllers/portfolioControllerDynamo.js
@@ -1,5 +1,5 @@
 const { v4: uuidv4 } = require("uuid");
-const { cloudinary } = require("../config/cloudinary");
+const { cloudinary } = require("../config/cloudinaryDynamo");
 const {
   DynamoDBDocumentClient,
   ScanCommand,
