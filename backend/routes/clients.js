cf99ebdb57ebd9f71c9944fead347ff734f4d7c1
prakash2816 <jayaprakashmekala56@gmail.com>
2025-11-20 09:16:39 +0530

    Update clients.js

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -2,28 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getClientsDynamo,
-  getClientDynamo,
-  createClientDynamo,
-  updateClientDynamo,
-  deleteClientDynamo
+  getClients,
+  getClient,
+  createClient,
+  updateClient,
+  deleteClient
 } = require('../controllers/clientControllerDynamo');
 
 const {
-  protectDynamo,
+  protect,
   optionalProtectDynamo,
   adminDynamo
 } = require('../middleware/authMiddlewareDynamo');
 
 router
   .route('/')
-  .get(optionalProtectDynamo, getClientsDynamo)
-  .post(protectDynamo, adminDynamo, createClientDynamo);
+  .get(optionalProtectDynamo, getClients)
+  .post(protect, admin, createClient);
 
 router
   .route('/:id')
-  .get(getClientDynamo)
-  .put(protectDynamo, adminDynamo, updateClientDynamo)
-  .delete(protectDynamo, adminDynamo, deleteClientDynamo);
+  .get(getClient)
+  .put(protect, admin, updateClient)
+  .delete(protect, admin, deleteClient);
 
 module.exports = router;
