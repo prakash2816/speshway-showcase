0aab539c6e727ff3fe11c741e995d40320a3b023
prakash2816 <jayaprakashmekala56@gmail.com>
2025-11-20 09:31:06 +0530

    Update sentences.js

--- backend/routes/sentences.js
+++ backend/routes/sentences.js
@@ -2,26 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getSentencesDynamo,
-  getSentenceDynamo,
-  createSentenceDynamo,
-  updateSentenceDynamo,
-  deleteSentenceDynamo
+  getSentences,
+  getSentence,
+  createSentence,
+  updateSentence,
+  deleteSentence
 } = require('../controllers/sentenceControllerDynamo');
 
-const { protectDynamo, adminDynamo } = require('../middleware/authMiddlewareDynamo');
+const { protect, admin } = require('../middleware/authMiddlewareDynamo');
 
 // Public routes
 router
   .route('/')
-  .get(getSentencesDynamo)
-  .post(createSentenceDynamo);
+  .get(getSentences)
+  .post(createSentence);
 
 // Admin protected routes
 router
   .route('/:id')
-  .get(getSentenceDynamo)
-  .put(protectDynamo, adminDynamo, updateSentenceDynamo)
-  .delete(protectDynamo, adminDynamo, deleteSentenceDynamo);
+  .get(getSentence)
+  .put(protect, admin, updateSentence)
+  .delete(protect, admin, deleteSentence);
 
 module.exports = router;
