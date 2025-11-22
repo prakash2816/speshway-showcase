43c86351e9fd04f1ec3df27483c01cf751b0db61
prakash2816 <jayaprakashmekala56@gmail.com>
2025-11-22 11:04:49 +0530

    Update portfolios.js

--- backend/routes/portfolios.js
+++ backend/routes/portfolios.js
@@ -14,7 +14,7 @@ const {
 const { protect } = require("../middleware/authMiddleware");
 
 // Multer Upload (Cloudinary Storage)
-const upload = require('../middleware/multerCloudinary');
+const { uploadPortfolioImageDynamo } = require("../config/cloudinaryDynamo");
 
 // ------------------------------------------------------------
 // GET ALL PORTFOLIOS (Public)
